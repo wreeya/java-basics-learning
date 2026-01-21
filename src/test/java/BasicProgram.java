100ba6721888a42159f34011ab565bd1ba5fa1b6
Riya Bhattarai <kan078bct065@kec.edu.np>
2026-01-21 23:33:47 +0545

    Learned to print the first sentence

---
+++ src/test/java/BasicProgram.java
@@ -0,0 +1,6 @@
+public class BasicProgram {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("This is a basic program");
+    }
+}
